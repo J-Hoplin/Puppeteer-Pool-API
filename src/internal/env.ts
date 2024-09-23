24eb39bd26ac318a40083d6b43fd1c4b357a129e
J-Hoplin <hoplin.dev@gmail.com>
2024-09-24 01:29:27 +0900

    Feat: Add body parser

--- src/internal/env.ts
+++ src/internal/env.ts
@@ -3,4 +3,4 @@ import { config } from 'dotenv';
 config();
 
 // Port number of express application. Default port is 3000
-export const APP_PORT = process.env.PORT || 3000;
+export const APP_PORT = process.env.PORT || 5700;
